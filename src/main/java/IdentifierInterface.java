dcc352fbd01cff0f4f818e7d2d1645ab45f41dce
Razvan Hutanu <razvangabrielh@gmail.com>
2018-10-15 14:01:24 +0200

    Identifier and Set ADT

---
+++ src/main/java/IdentifierInterface.java
@@ -0,0 +1,7 @@
+public interface IdentifierInterface {
+    public void add (char toAdd);
+    public boolean equals (StringBuffer identifier);
+    public StringBuffer getIdentifier();
+    public int getSize();
+    public void print();
+}
