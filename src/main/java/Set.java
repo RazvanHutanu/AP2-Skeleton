b0ce3e7e3f269298a98d81b50d556a54a481cbf0
Razvan Hutanu <razvangabrielh@gmail.com>
2018-10-16 19:13:05 +0200

    Set class and expressions

--- src/main/java/Set.java
+++ src/main/java/Set.java
@@ -1,3 +1,97 @@
-public class Set {
+public class Set extends List implements SetInterface<StringBuffer>{
+    private ListInterface<StringBuffer> set = new List<>();
+
+    Set(){
+        set.init();
+    }
+
+    @Override
+    public void add(StringBuffer value) {
+        set.insert(value);
+    }
+
+    @Override
+    public ListInterface<StringBuffer> getList(){
+        return set.copy();
+    }
+
+    @Override
+    public int getSize(){
+        return set.size();
+    }
+
+    @Override
+    public SetInterface<StringBuffer> difference(SetInterface<StringBuffer> list){
+        ListInterface<StringBuffer> set2 = getList();
+        SetInterface<StringBuffer> aux = new Set();
+
+        set.goToFirst();
+
+        do{
+            if(! set2.find(set.retrieve()))
+                aux.add(set.retrieve());
+        }while(set.goToNext());
+
+        return aux;
+    }
+
+    @Override
+    public SetInterface<StringBuffer> intersection(SetInterface<StringBuffer> list){
+        ListInterface<StringBuffer> set2 = getList();
+        SetInterface<StringBuffer> aux = new Set();
+
+        set.goToFirst();
+
+        do{
+            if(set2.find(set.retrieve()))
+                aux.add(set.retrieve());
+        }while(set.goToNext());
+
+        return aux;
+    }
+
+    @Override
+    public SetInterface<StringBuffer> union(SetInterface<StringBuffer> list) {
+        ListInterface<StringBuffer> set2 = getList();
+        SetInterface<StringBuffer> aux = new Set();
+
+        set.goToFirst();
+
+        do{
+            aux.add(set.retrieve());
+        }while(set.goToNext());
+
+        set2.goToFirst();
+
+        do{
+            if(! set.find(set2.retrieve()))
+                aux.add(set2.retrieve());
+        }while(set2.goToNext());
+
+        return aux;
+    }
+
+    @Override
+    public SetInterface<StringBuffer> symmetricDifference(SetInterface<StringBuffer> list) {
+        ListInterface<StringBuffer> set2 = getList();
+        SetInterface<StringBuffer> aux = new Set();
+
+        set.goToFirst();
+
+        do{
+            if(! set2.find(set.retrieve()))
+                aux.add(set.retrieve());
+        }while(set.goToNext());
+
+        set2.goToFirst();
+
+        do{
+            if(! set.find(set2.retrieve()))
+                aux.add(set2.retrieve());
+        }while(set2.goToNext());
+
+        return aux;
+    }
+
 
 }
--- src/main/java/SetInterface.java
+++ src/main/java/SetInterface.java
@@ -1,2 +1,10 @@
-public interface SetInterface {
+public interface SetInterface<E> {
+    void add(E value);
+    int getSize();
+    boolean isEmpty();
+    ListInterface<StringBuffer> getList();
+    SetInterface<E> difference(SetInterface<E> set2);
+    SetInterface<E> intersection(SetInterface<E> list);
+    SetInterface<E> union(SetInterface<E> list);
+    SetInterface<E> symmetricDifference(SetInterface<E> list);
 }
